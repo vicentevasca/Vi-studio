755f56e01fbcfc474285e8972ce427cf27897252
Vicente Vásquez Caro <vice.vasca@gmail.com>
2026-02-22 17:43:04 -0300

    firebase client

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -1,27 +1,30 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import { getAnalytics } from "firebase/analytics";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  // Aquí pones las credenciales de tu proyecto original
-  const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "tu-proyecto.firebaseapp.com",
-    projectId: "tu-proyecto",
-    storageBucket: "tu-proyecto.appspot.com",
-    messagingSenderId: "TUS_DATOS",
-    appId: "TUS_DATOS"
-  };
+    // Aquí pones las credenciales de tu proyecto original
+    const firebaseConfig = {
+        apiKey: "AIzaSyAoUXKgCVTVeapOIhFAx_tQSyp9etT4M34",
+        authDomain: "vi-studio-core.firebaseapp.com",
+        projectId: "vi-studio-core",
+        storageBucket: "vi-studio-core.firebasestorage.app",
+        messagingSenderId: "567597957944",
+        appId: "1:567597957944:web:69959b423bca709a2db7cd",
+        measurementId: "G-W6C0BCYES1"
+    };
 
-  const app = initializeApp(firebaseConfig)
-  const auth = getAuth(app)
-  const db = getFirestore(app)
+    const app = initializeApp(firebaseConfig)
+    const auth = getAuth(app)
+    const db = getFirestore(app)∫
+    const analytics = getAnalytics(app);
 
-  // Esto hace que Firebase esté disponible globalmente como $auth y $db
-  return {
-    provide: {
-      auth,
-      db
+    // Esto hace que Firebase esté disponible globalmente como $auth y $db
+    return {
+        provide: {
+            auth,
+            db
+        }
     }
-  }
 })
